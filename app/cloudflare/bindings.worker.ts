aee64a42437a9e6c527eb6444c365bbce334811f
davitti <abuadonald@gmail.com>
2025-08-19 23:24:11 +0100

    Add type definitions for Bindings and Hyperdrive in bindings.worker.ts

---
+++ app/cloudflare/bindings.worker.ts
@@ -0,0 +1,16 @@
+export type Bindings = {
+	DATABASE_URL: string;
+	PORTFOLIO_HYPERDRIVE: Hyperdrive;
+	RESEND_APIKEY: string;
+	RESEND_FROM: string;
+	ORIGIN: string;
+	AUTH_SECRET: string;
+};
+export type Hyperdrive = {
+	connectionString: string;
+	port: number;
+	host: string;
+	password: string;
+	user: string;
+	database: string;
+};
