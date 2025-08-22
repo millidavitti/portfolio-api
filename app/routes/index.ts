a7423de75a8a849177f5cab138d0bf8abbd5bc0a
davitti <abuadonald@gmail.com>
2025-08-22 19:17:09 +0100

    refactor: Remove CSRF middleware from routes

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -2,16 +2,10 @@ import { Hono } from "hono";
 import api from "./api";
 import auth from "./auth";
 import { Bindings } from "app/cloudflare/bindings.worker";
-import { csrf } from "hono/csrf";
 
 const routes = new Hono<{ Bindings: Bindings }>();
 
 routes.route("/auth", auth);
-routes.use(
-	csrf({
-		origin: ["http://localhost:3000"],
-	}),
-);
 routes.route("/api", api);
 
 export default routes;
