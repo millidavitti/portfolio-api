d3a1b8f2e72ef058012a46d0824ccdcea1fb8a6a
davitti <abuadonald@gmail.com>
2025-08-24 13:42:54 +0100

    feat: Add socials route to API version 1 and update bindings type for consistency

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,9 +1,9 @@
 import { Hono } from "hono";
 import api from "./api";
 import auth from "./auth";
-import { Bindings } from "app/cloudflare/bindings.worker";
+import { WorkerBindings } from "app/cloudflare/bindings.worker";
 
-const routes = new Hono<{ Bindings: Bindings }>();
+const routes = new Hono<{ Bindings: WorkerBindings }>();
 
 routes.route("/auth", auth);
 routes.route("/api", api);
